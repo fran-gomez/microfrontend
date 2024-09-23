cd873022504c9892b6b26366fbd337c81c144a86
Francisco Gomez <fgvolonterio@gmail.com>
2024-09-23 19:59:30 -0300

    sec

--- src/MiCompEntretenimiento.js
+++ src/MiCompEntretenimiento.js
@@ -4,9 +4,6 @@ function MiCompEntretenimiento() {
     // return a jsx ul list of a menu with five random items
     return <div>
        <p>LucasFilms anuncia una trilogia centrada en Jar Jar Binks</p>
-
-        
-
     </div>;
 }
 
